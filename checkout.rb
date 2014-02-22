2e94c28df8cbad9f0bc8f494cd5c3e3f00774d6c
Juan Pieres <jpieres@gmail.com>
2014-02-22 17:46:29 +0100

    Add scan method

--- checkout.rb
+++ checkout.rb
@@ -1,2 +1,6 @@
 class Checkout
+
+  def scan
+  end
+  
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,5 +3,15 @@ require 'minitest/spec'
 require_relative '../checkout'
 
 describe Checkout do
-  
+
+  it "can create an instance of Checkout class" do
+    co = Checkout.new
+    co.must_be_instance_of Checkout
+  end
+
+  it "should respond to scan method" do
+    co = Checkout.new
+    co.must_respond_to :scan
+  end
+
 end
