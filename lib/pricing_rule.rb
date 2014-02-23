7f09f60c6226a959b21e300335e0cc2b40940029
Juan Pieres <jpieres@gmail.com>
2014-02-23 21:37:08 +0100

    Add more doc comments

--- lib/pricing_rule.rb
+++ lib/pricing_rule.rb
@@ -1,5 +1,17 @@
 class PricingRule
-
+  # Attribute      code: Item code used to define the which elements will have their
+  #                      prices updated.
+  # Attribute condition: should be passed a Proc with a enumerable collection of objects
+  #                      as parameter. Each object in the collection should respond to
+  #                      messages code and price. Its return value is used to indicate
+  #                      whether the condition on the collection elements is met
+  #                      or not.
+  # Attribute    action: should be passed a Proc with a enumerable collection of objects
+  #                      as parameter. Each object in the collection should respond to
+  #                      messages code and price. If the condition is met, the action will 
+  #                      update the prices in the collection with the criteria defined in
+  #                      the Proc.
+  
   attr_accessor :code, :condition, :action
   def initialize(args = {})
     @code = args[:code]
--- spec/pricing_rule_spec.rb
+++ spec/pricing_rule_spec.rb
@@ -35,11 +35,17 @@ describe PricingRule do
   end
 
   # TODO add more specs to test and document :condition and :action attributes
+  # Attribute      code: Item code used to define the which elements will have their
+  #                      prices updated.
   # Attribute condition: should be passed a Proc with a enumerable collection of objects
   #                      as parameter. Each object in the collection should respond to
-  #                      messages code and price.
+  #                      messages code and price. Its return value is used to indicate
+  #                      whether the condition on the collection elements is met
+  #                      or not.
   # Attribute    action: should be passed a Proc with a enumerable collection of objects
   #                      as parameter. Each object in the collection should respond to
-  #                      messages code and price.
+  #                      messages code and price. If the condition is met, the action will 
+  #                      update the prices in the collection with the criteria defined in
+  #                      the Proc.
 
 end
